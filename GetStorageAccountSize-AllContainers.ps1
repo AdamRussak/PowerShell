cf765ebcf0a3822907f4cee788c14a5845219c3f
AdamRussak <31593022+AdamRussak@users.noreply.github.com>
2019-08-15 11:44:33 +0300

    get storage account for all containers in storage

---
+++ GetStorageAccountSize-AllContainers.ps1
@@ -0,0 +1,39 @@
+<#
+Writen By: Adam Russak
+Version: 1.0.1v
+
+Description: 
+    - Script assume you are connected to Azure
+    - Script uses AZ PowerShell Module
+    - You will need to provide Resouce Group And Storage Account to the CLI 
+#>
+
+Function Format-FileSize() {
+    Param ([object]$BlobSize)
+    If ($BlobSize -gt 1TB) {[string]::Format("{0:0.00} TB", $BlobSize / 1TB)}
+    ElseIf ($BlobSize -gt 1GB) {[string]::Format("{0:0.00} GB", $BlobSize / 1GB)}
+    ElseIf ($BlobSize -gt 1MB) {[string]::Format("{0:0.00} MB", $BlobSize / 1MB)}
+    ElseIf ($BlobSize -gt 1KB) {[string]::Format("{0:0.00} kB", $BlobSize / 1KB)}
+    ElseIf ($BlobSize -gt 0) {[string]::Format("{0:0.00} B", $BlobSize)}
+    Else {""}
+    }
+
+$rgName = Read-Host "Provide a Resource Group Name"
+$storageAccountName = Read-Host "Provide Storage Account"
+$SASkey = (Get-AzStorageAccountKey -ResourceGroupName $rgName -AccountName $storageAccountName).Value[0] 
+$destinationContext = New-AzStorageContext –StorageAccountName $storageAccountName -StorageAccountKey $SASkey
+$Containers = Get-AzStorageContainer -Context $destinationContext
+ForEach ($CList in $Containers) {
+    $BlobList = (Get-AzStorageBlob -Context $destinationContext -Container $CList.Name)
+            ForEach ($Blob in $BlobList){
+                $blobname = (Get-AzStorageBlob -Context $destinationContext -Container $CList.Name) | Where-Object{$_.Name -like $blob.Name}
+                $bloburl = $blobname.ICloudBlob.uri.AbsoluteUri
+                $containerName = $bloburl.Split("{/}")[3]
+                $BlobSize = Format-FileSize($Blob.Length)
+                [PSCustomObject]@{
+                    "Container" = $containerName
+                    "Blob Name" = $blobname.Name
+                    "Size (MB)" = $BlobSize
+                    }
+                }
+    }
