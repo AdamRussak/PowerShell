06552f8ae24921edc163678edb72f05e0f6a0dab
AdamRussak <31593022+AdamRussak@users.noreply.github.com>
2019-11-11 00:22:19 +0200

    minor changes

--- fullscriptgenerelized v1.1.ps1
+++ fullscriptgenerelized v1.1.ps1
@@ -11,7 +11,6 @@ About: This Script Creates a Generlized Image From VM to Storage VHD
         - Create VHD from Sapshoot & transfer to Storage-Account
         - Check Transfer Status
 #>
-
 ###########
 #Functions#
 ###########
@@ -38,58 +37,43 @@ function Login
             throw
         }
     }
-
     if ($needLogin)
     {
         Login-AzAccount
     }
 }
-
-
 #run Func to see login status
 Login
 ##################
 #Global-Variables#
 ##################
-
 #login Subscription
 $Subscription = Read-Host -Prompt 'Input the Subscription ID'
-
 #Resources Group
 $defaultRG = Read-Host -Prompt 'Input the Resource Group'
-
 #VM Name
 $VMNAME = Read-Host -Prompt 'Input the VM name'
-
 #snapshoot Name
 $snapshotName = Read-Host -Prompt 'Input Snapshoot Desired name'
-
 #Vm location
 $location = Read-Host -Prompt 'Input the Region you need to use'
-  
 ##Provide storage account name where you want to copy the snapshot. 
 $storageAccountName = Read-Host -Prompt 'Input the Storage Account Name'
-
 #Name of the storage container where the downloaded snapshot will be stored
 $storageContainerName = Read-Host -Prompt 'Input the Container name'
-
 #Provide the key of the storage account where you want to copy snapshot. 
 $storageAccountKey = Read-Host -Prompt 'Input the Storage Account Key'
-
 #Provide the name of the VHD file to which snapshot will be copied.
 $destinationVHDFileName = Read-Host -Prompt 'Input the VHD name'
-
 #Provide Shared Access Signature (SAS) expiry duration in seconds e.g. 3600.
 #Know more about SAS here: https://docs.microsoft.com/en-us/azure/storage/storage-dotnet-shared-access-signature-part-1
 $sasExpiryDuration = "36000"
-
 #####################
 ###Start of Script###
 #####################
 #runtime for the script
 $startTime = (Get-Date).Minute
 Set-AzContext -SubscriptionId $Subscription
-
 ##############################
 #Deallocate the VM resources#
 ##############################
@@ -103,7 +87,6 @@ $vm.Statuses
 #End of Deallocation Time output
 $EndTime1 = (Get-Date).Minute
 Write-Host "The Deallocation Took $($EndTime1 - $startTime1) Minutes to Run"
-
 ####################
 # create-Snapshoot #
 ####################
@@ -116,7 +99,6 @@ New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupNam
 #End of Snapshoot Time output
 $EndTime2 = (Get-Date).Minute
 Write-Host "The Snapshoot Took $($EndTime2 - $startTime2) Minutes to Run"
-
 ##########################
 #create VHD from Sapshoot#
 ##########################
@@ -132,12 +114,10 @@ $EndTime3 = (Get-Date).Minute
 Write-Host "Creating the VHD Took $($EndTime3 - $startTime3) Minutes to Run"
 $EndTime = (Get-Date).Minute
 Write-Host "The Script Took $($EndTime - $startTime) Minutes to Run"
-
 #######################
 #Check VHD Copy Status#
 #######################
 Get-AzStorageBlobCopyState -Blob $destinationVHDFileName -Container $storageContainerName -Context $destinationContext -WaitForComplete
-
 ###################
 ###End Of Script###
 ###################
